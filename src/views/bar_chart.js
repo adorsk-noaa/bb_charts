c8951e03641ff9311c04529444d4497c1753544a
adorsk <adorsk>
2012-05-03 10:04:31 -0400

    added explicit resizing of grid ticks, for dealing w/ scrollbars

--- src/views/bar_chart.js
+++ src/views/bar_chart.js
@@ -51,30 +51,65 @@ function($, Backbone, _, _s, ChartView, SimpleInterpreter, template, row_templat
 			var zero_pos = (0 - this.vmin)/this.vrange * 100;
 
 			_.each(this.model.get('data'), function(datum){
-				var datum_value = datum.data[0].value;
-				var datum_pos  = (datum.data[0].value - this.vmin)/this.vrange * 100;
-				var css_pos;
-				if ( datum_pos < zero_pos ){
-					css_pos = _s.sprintf('right: %.1f%%', 100 - zero_pos);
+				var value = parseFloat(datum.data[0].value);
+				var width = 'width: 0;';
+				var visibility = 'visibility: hidden;';
+				var position = '';
+				var should_draw = false;
+
+				// Zero is off-plot, to the left.
+				if (this.vmin >= 0){
+					if (value > this.vmin){
+						should_draw = true;
+					}
 				}
-				else{
-					css_pos = _s.sprintf('left: %.1f%%', zero_pos);
+				// Zero is in the plot.
+				else if (this.vmin <= 0  && this.vmax >= 0){
+					should_draw = true;
 				}
+				// Zero is off-plot, to the right.
+				else if (this.vmax <= 0){
+					if (value <= this.vmax){
+						should_draw = true;
+					}
+				}
+
+				if (should_draw){
+					visibility = 'visibility: visible;';
+
+					if (value > 0){
+						var left = Math.max(this.vmin, 0);
+						width = _s.sprintf('width: %.1f%%;', (value - left)/this.vrange * 100);
+						position = _s.sprintf('left: %.1f%%;', (left - this.vmin)/this.vrange * 100);
+					}
+					else{
+						var right = Math.min(this.vmax, 0);
+						width = _s.sprintf('width: %.1f%%;', (right - value)/this.vrange * 100);
+						position = _s.sprintf('right: %.1f%%;', 100 - (right - this.vmin)/this.vrange * 100);
+					}
+				}
+
 				$row = $(_.template(row_template, {
-					'label': datum.label,
-					'css_pos': css_pos,
-					'width': Math.abs(datum_value)/this.vrange * 100
+					//'label': datum.label,
+					'label': value,
+					'visibility': visibility,
+					'width': width,
+					'position': position
 				}));
 				$container.append($row);
 
 			}, this);
-			
+
 			this.renderGrid();
 		},
 
 		renderGrid: function(){
 			var $grid = $('.chart-grid', this.el);
 			$grid.html('');
+
+			// Explicilty resize grid because scrollbars alter the chart width.
+			var image_margin = $('.chart-image-wrapper', this.el).width() - $('.chart-image', this.el).width();
+			$('.chart-grid-wrapper .inner', this.el).css('marginRight', image_margin);
 
 			_.each(this.model.get('grid_line_positions'), function(pos){
 				var label = _s.sprintf("%.1f", this.vmin + pos * this.vrange);
