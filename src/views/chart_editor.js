d1a84e93dc2f8d2ce9a36f7f382a4edb3209d9d4
adorsk <adorsk>
2012-05-09 12:27:34 -0400

    added expansion/contraction of axis field panels

--- src/views/chart_editor.js
+++ src/views/chart_editor.js
@@ -4,15 +4,15 @@ define([
 	"use!underscore",
 	"use!ui",
 	"_s",
-	"./single_field_selector",
-	"./value_field",
 	"text!./templates/chart_editor.html"
 		],
-function($, Backbone, _, ui, _s, SingleFieldSelectorView, ValueFieldView, template){
+function($, Backbone, _, ui, _s, template){
 
 	var ChartEditorView = Backbone.View.extend({
 
 		events: {
+			'click .category-field-container .title': 'toggleCategoryField',
+			'click .quantity-field-container .title': 'toggleQuantityField'
 		},
 
 		initialize: function(){
@@ -22,153 +22,71 @@ function($, Backbone, _, ui, _s, SingleFieldSelectorView, ValueFieldView, templa
 
 		render: function(){
 			$(this.el).html(_.template(template, {}));
+			this.$lp_el = $('.left-panel', this.el);
+			this.$qfc_el = $('.quantity-field-container', this.el);
+		},
 
-			// Add the chart.
-			$('.chart-wrapper', this.el).append(this.model.get('chart').el);
-			
-			var ds = this.model.get('datasource');
-			var schema = ds.get('schema');
-
-			this.left_panel_el = $('.left.panel', this.el);
-			this.right_panel_el = $('.right.panel', this.el);
-
-			// Make left panel resizable.
-			var _this = this;
-			$(this.left_panel_el).resizable({
-				minWidth: $(_this.left_panel_el).css('minWidth'),
-				handles: 'e',
-				stop: function(event, ui) {
-					$(_this.left_panel_el).css('height', '100%');
-					_this.resizeRightPanel();
-				},
-				resize: function(event, ui){
-					this.setRightPanelLeftEdge();
-				}
-			});
-
-			//  Add category field selector.
-			var category_field_model = new Backbone.Model({
-				field_definitions: schema.get('category_fields')
-			});
-			this.model.set('category_field', category_field_model);
-			var category_field_selector = new SingleFieldSelectorView({
-				el: $('.category-field', this.el),
-				model: category_field_model
-			});
-
-			//  Add value field selector.
-			var value_field_model = new Backbone.Model({
-				field_definitions: schema.get('value_fields')
-			});
-			this.model.set('value_field', value_field_model);
-			var value_field_selector = new SingleFieldSelectorView({
-				el: $('.value-field', this.el),
-				model: value_field_model,
-				fieldViewClass: ValueFieldView
-			});
-
-			// Fetch new data when the datasource query changes.
-			ds.get('query').on('change', ds.fetch, ds);
-
-			// Handle changes in datasource data.
-			ds.on('change:data', this.onDatasourceDataChange, this);
-
-			// Change the datasource query when the field selectors change.
-			category_field_model.on('change:selected_field', this.onCategoryFieldChange, this);
-			value_field_model.on('change:selected_field', this.onValueFieldChange, this);
-
-			// Set the initial default fields.
-			var category_fields = schema.get('category_fields');
-			if (category_fields && category_fields.length > 0){
-				category_field_selector.setSelectedField(category_fields[0].id);
+		toggleCategoryField: function(){
+			if (! this.$lp_el.hasClass('changing')){
+				this.expandContractLeftPanel({expand: ! this.$lp_el.hasClass('expanded')});
 			}
-			var value_fields = schema.get('value_fields');
-			if (value_fields && value_fields.length > 0){
-				value_field_selector.setSelectedField(value_fields[0].id);
+		},
+
+		toggleQuantityField: function(){
+			if (! this.$qfc_el.hasClass('changing')){
+				this.expandContractQuantityField({expand: ! this.$qfc_el.hasClass('expanded')});
 			}
-
 		},
 
-		onCategoryFieldChange: function(){
-			$('.category_field', this.el).html(this.model.get('category_field').get('selected_field').get('label'));
-			this.updateDatasourceQuery()
+		expandContractLeftPanel: function(opts){
+			var expand = opts.expand;
+			var $lp_el = $('.left-panel', this.el);
+			var $rp_el = $('.right-panel', this.el);
+			var width_attr = (expand) ? 'maxWidth' : 'minWidth';
+
+			$lp_el.addClass('changing');
+			$rp_el.animate(
+					{
+						left: $lp_el.css(width_attr)
+					},
+					{
+						complete: function(){
+							$lp_el.removeClass('changing');
+							if (expand){
+								$lp_el.addClass('expanded')
+							}
+							else{
+								$lp_el.removeClass('expanded')
+							}
+						}
+					}
+			);
 		},
 
-		onValueFieldChange: function(){
-			this.updateDatasourceQuery()
-		},
+		expandContractQuantityField: function(opts){
+			var expand = opts.expand;
+			var $qfc_el = $('.quantity-field-container', this.el);
+			var $qfb_el = $('.body-container', $qfc_el);
+			var height_attr = (expand) ? 'maxHeight' : 'minHeight';
 
-		updateDatasourceQuery: function(){
-			this.model.get('datasource').get('query').set({
-				'CATEGORY_FIELDS': [this.model.get('category_field').get('selected_field')],
-				'VALUE_FIELDS': [this.model.get('value_field').get('selected_field')]
-			});
-		},
-
-		onDatasourceDataChange: function(){
-			console.log('onDatasourceDatachange');
-			// Set chart data.
-			this.model.get('chart').set('data', this.model.get('datasource').get('data'));
-		},
-
-		getBounds: function(el){
-			var pos = $(el).position();
-			var width = $(el).outerWidth(true);
-			var height = $(el).outerHeight(true);
-			return {
-				top: pos.top, 
-				right: pos.left + width, 
-				bottom: pos.top + height, 
-				left: pos.left,
-				width: width,
-				height: height
-			};
-		},
-
-		resize: function(){
-			// Resize inner container.
-			var inner_el = $(this.el).children('.inner');
-			var container = inner_el.parent();
-			var totalHeight = container.height();
-			console.log(inner_el);
-			inner_el.css('height', totalHeight);
-
-			// Resize panels.
-			this.resizeLeftPanel();
-			this.resizeRightPanel();
-		},
-
-		resizeLeftPanel: function() {
-			var el = this.left_panel_el;
-			this.fillContainerHeight(el);
-
-			var inner_el = $(this.el).children('.inner');
-			var container = inner_el.parent();
-			var totalHeight = container.height();
-
-		},
-
-		resizeRightPanel: function() {
-			var el = this.right_panel_el
-			this.fillContainerHeight(el);
-
-			this.setRightPanelLeftEdge();
-
-			var container = el.parent();
-			var totalWidth = container.width();
-			var left_panel_width = $(this.left_panel_el).width();
-			el.css('width', totalWidth - left_panel_width);
-		},
-
-		setRightPanelLeftEdge: function(){
-			var left_panel_bounds = this.getBounds(this.left_panel_el);
-			$(this.right_panel_el).css('left', left_panel_bounds.right);
-		},
-
-		fillContainerHeight: function(el){
-			var container = $(el).parent();
-			$(el).css('height', container.height());
-		},
+			$qfb_el.addClass('changing');
+			$qfb_el.animate(
+					{
+						height: $qfb_el.css(height_attr)
+					},
+					{
+						complete: function(){
+							$qfb_el.removeClass('changing');
+							if (expand){
+								$qfc_el.addClass('expanded')
+							}
+							else{
+								$qfc_el.removeClass('expanded')
+							}
+						}
+					}
+			);
+		}
 
 	});
 
